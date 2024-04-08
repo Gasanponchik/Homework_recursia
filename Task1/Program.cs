4ea1a4decd5437b53c9bb0a5ef2ef2b8da5013fe
Gasan <1804islamov.gasan@gmail.com>
2024-04-08 11:11:22 +0500

    Удалил решение 2ой задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -17,18 +17,5 @@ return Digit(firstNum + 1,theEndNum);
 System.Console.WriteLine(Digit(firstNum,theEndNum));
 
 
-// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-int Ack(int n, int m)
-{
-if (n == 0) return m + 1;
-else if (m == 0) return Ack(n - 1, 1);
-else return Ack(n - 1, Ack(n,m - 1));
-}
-
-System.Console.Write("Введите число n: ");
-int n = int.Parse(Console.ReadLine()!);
-System.Console.Write("Введите число m: ");
-int m = int.Parse(Console.ReadLine()!);
-System.Console.WriteLine(Ack(n,m));
 
